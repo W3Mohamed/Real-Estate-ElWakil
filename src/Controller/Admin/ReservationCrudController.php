32514f4ca4a39bbb4a6d20027f48f727c3385f2a
Mohamed <medmohdz181@gmail.com>
2025-04-24 21:57:19 +0100

    fonctionnement de reservation

--- src/Controller/Admin/ReservationCrudController.php
+++ src/Controller/Admin/ReservationCrudController.php
@@ -6,9 +6,9 @@ use App\Entity\Reservation;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ReservationCrudController extends AbstractCrudController
@@ -23,7 +23,7 @@ class ReservationCrudController extends AbstractCrudController
         return $crud
             ->setPageTitle('index', 'Réservations')
             ->setPageTitle('detail', 'Détail réservation')
-            ->setSearchFields(['nom', 'email', 'telephone'])
+            ->setSearchFields(['nom', 'email', 'telephone', 'bien.libelle'])
             ->setDefaultSort(['id' => 'DESC'])
             ->showEntityActionsInlined();
     }
@@ -33,7 +33,8 @@ class ReservationCrudController extends AbstractCrudController
         return $actions
             ->disable('new', 'edit') // Désactive création et modification
             ->add('index', 'detail') // Ajoute le bouton "Voir"
-            ->setPermission('delete', 'ROLE_ADMIN'); // Restreint la suppression aux admins
+            ->setPermission('delete', 'ROLE_ADMIN') // Restreint la suppression aux admins
+            ->remove('detail', 'delete'); // Optionnel: supprime le bouton delete de la page detail
     }
 
     public function configureFields(string $pageName): iterable
@@ -43,7 +44,18 @@ class ReservationCrudController extends AbstractCrudController
             TextField::new('nom', 'Nom complet'),
             TextField::new('email'),
             TextField::new('telephone', 'Téléphone'),
-            TextareaField::new('message')->onlyOnDetail()
+            AssociationField::new('Bien')
+                ->setLabel('Bien concerné')
+                ->setCrudController(BienCrudController::class) // Assurez-vous d'importer le BienCrudController
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getBien() ? $entity->getBien()->getLibelle() : 'Non spécifié';
+                }),
+            TextareaField::new('message')
+                ->onlyOnDetail()
+                ->setLabel('Message de la réservation')
+                ->formatValue(function ($value) {
+                    return nl2br($value); // Pour afficher correctement les sauts de ligne
+                })
         ];
     }
 }
